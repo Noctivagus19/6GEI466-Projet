54f992942d14d7f121ef3aad7bbd947cb3d7a002
JM Plourde <jmichel.plourde@gmail.com>
2021-03-13 16:29:34 -0500

    Put iss pos call lon lat to hidden inputs
    Needed for the openlayer script to collect the data and use it
    to display the pos on the map.

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    # http = urllib3.PoolManager()
-    # r = http.request('GET', 'http://api.open-notify.org/iss-now.json')
-    # obj = json.loads(r.data)
-    # longitude = obj['iss_position']['longitude']
-    # latitude = obj['iss_position']['latitude']
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://api.open-notify.org/iss-now.json')
+    obj = json.loads(r.data)
+    longitude = obj['iss_position']['longitude']
+    latitude = obj['iss_position']['latitude']
 
-    return render_template('accueil.html')
+    return render_template('accueil.html', issLon=longitude, issLat=latitude)
