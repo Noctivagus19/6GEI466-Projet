29ab26ab8a395984ceb11ba5db6c41807f64f268
giant995 <giant995@users.noreply.github.com>
2021-03-17 20:06:03 -0400

    Add API calls for astros in space and wikipedia
    For each astronaut onboard the ISS, there's a call to its wikipedia page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import json
 import urllib3
+import urllib
+from urllib.parse import urlencode, quote_plus
 
 app = Flask(__name__)
 
@@ -8,9 +10,51 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     http = urllib3.PoolManager()
+
+    # Where is the ISS now
     r = http.request('GET', 'http://api.open-notify.org/iss-now.json')
     obj = json.loads(r.data)
     longitude = obj['iss_position']['longitude']
     latitude = obj['iss_position']['latitude']
 
-    return render_template('accueil.html', issLon=longitude, issLat=latitude)
+    # Who are in space right now
+    r = http.request('GET', 'http://api.open-notify.org/astros.json')
+    obj = json.loads(r.data)
+    iss_astronauts = []
+    for astro in obj["people"]:
+        if astro["craft"] == 'ISS':
+            astro['wiki_page'] = search_wikipedia(astro['name'])
+            iss_astronauts.append(astro)
+
+    return render_template(
+        'accueil.html',
+        issLon=longitude,
+        issLat=latitude,
+        astros=iss_astronauts,
+    )
+
+
+def search_wikipedia(term):
+    http = urllib3.PoolManager()
+    payload = {
+        'action': 'query',
+        'list': 'search',
+        'prop': 'pageimages',
+        'srsearch': f'{term} astronaut',
+        'format': 'json',
+    }
+    payload = urlencode(payload, quote_via=urllib.parse.quote)
+    wiki_url = 'http://en.wikipedia.org/'
+    link = f'{wiki_url}w/api.php?{payload}'
+    r = http.request('GET', link)
+    obj = json.loads(r.data)
+
+    wiki_page = {}
+
+    if len(obj['query']['search']) > 0:
+        first_result = obj['query']['search'][0]
+        page_title = str.replace(first_result['title'], ' ', '_')
+        wiki_page['url'] = f'https://en.wikipedia.org/wiki/{page_title}'
+        wiki_page['snippet'] = first_result['snippet']
+
+    return wiki_page
