85d52b93512911241f1f8f87d789b7f0aa89e19e
giant995 <giant995@users.noreply.github.com>
2021-04-05 16:43:43 -0400

    Remove API calls

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import (
     Flask,
     render_template,
@@ -5,47 +7,33 @@ from flask import (
 
 import json
 import urllib3
-import urllib
-from urllib.parse import urlencode, quote_plus
-from ip2geotools.databases.noncommercial import DbIpCity
-from requests import get
-from datetime import datetime
 
 app = Flask(__name__)
+
+
+@app.template_filter()
+def format_datetime(value):
+    return datetime.fromtimestamp(value/1000.0).strftime('%Y-%m-%d at %H:%M:%S')
 
 
 @app.route('/', methods=['GET'])
 def index():
     http = urllib3.PoolManager()
 
-    # Where is the ISS now
-    r = http.request('GET', 'http://api.open-notify.org/iss-now.json')
-    obj = json.loads(r.data)
-    longitude = obj['iss_position']['longitude']
-    latitude = obj['iss_position']['latitude']
-
-    # ISS Pass times
-    myIp = get('https://api.ipify.org').text
-    infoIp = DbIpCity.get(myIp, api_key='free')
-    myLocation = infoIp.city + ', ' + infoIp.region + ', ' + infoIp.country
-    r = http.request('GET', 'http://api.open-notify.org/iss-pass.json?lat=' + str(infoIp.latitude) + '&lon=' + str(
-        infoIp.longitude) + '')
-    obj = json.loads(r.data)
-    iss_risetimes = []
-    for response in obj["response"]:
-        print(response)
-        iss_risetimes.append(datetime.fromtimestamp(response['risetime']))
-
     r = http.request('GET', '127.0.0.1:8080/api/v1/iss/astronauts')
     iss_astronauts = json.loads(r.data)
 
+    r = http.request("GET", "127.0.0.1:8080/api/v1/iss/positions")
+    iss_positions = json.loads(r.data)
+
+    r = http.request("GET", "127.0.0.1:8080/api/v1/iss/pass-times")
+    iss_pass_time = json.loads(r.data) if r.status == 200 else None
+
     return render_template(
         'accueil.html',
-        issLon=longitude,
-        issLat=latitude,
+        positions=iss_positions,
         astros=iss_astronauts,
-        issRisetimes=iss_risetimes,
-        infoLocation=myLocation
+        pass_time=iss_pass_time,
     )
 
 
